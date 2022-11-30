a9252d07ccaf93c2e1e257fff60c44278fd0ddf0
Andy <andythomas.py@gmail.com>
2022-11-30 15:07:01 -0600

    first save with class based views added

--- reviews/urls.py
+++ reviews/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.reviews),
+    path("", views.ReviewView.as_view()),    # need to add as_view() since we are calling "View" class in views when creating out class
     path("thank-you", views.thank_you, name="thank-you"),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,32 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
+from django.views import View
 
 from .forms import ReviewForm
 from .models import Review
 
 # Create your views here.
 
-def reviews(request):
-    if request.method == "POST":
-        existing_data = Review.objects.get(pk=2) # assign variable to entry which should be updated
-        print(existing_data)
-        form = ReviewForm(request.POST, instance=existing_data) # to update data entry include instance keyword and pass in entry to update
+class ReviewView(View):
+    def get(self, request):
+        form = ReviewForm()
 
-        # form = ReviewForm(request.Post) # to add a new entry
+        return render(request, "reviews/review.html", {
+            "form": form
+        })
+
+    def post(self, request):
+        form = ReviewForm(request.POST)
 
         if form.is_valid():  # is_valid will do 3 things.. 1) validiate the inputs (by default verifies not empty) 2) then will return True if valid 3) if data is valid it populates another field with valide data
             print(form)
             form.save()
 
-            
             return HttpResponseRedirect("/thank-you")
-
-    else:
-        form = ReviewForm()
-
-    return render(request, "reviews/review.html", {
-        "form": form
-    })
+        
+        return render(request, "reviews/review.html", {
+            "form": form
+        })
 
 
 def thank_you(request):
