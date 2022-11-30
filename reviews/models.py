73e355241d4e38c1e7be7f9fa368ca717028cf5c
Andy <andythomas.py@gmail.com>
2022-11-29 22:24:42 -0600

    Notes for seperate model and forms

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Review(models.Model):
+    user = models.CharField(max_length=20)
+    review = models.TextField()
+    rating = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.user} {self.review} {self.rating}"
--- reviews/views.py
+++ reviews/views.py
@@ -2,15 +2,26 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
 
 from .forms import ReviewForm
+from .models import Review
 
 # Create your views here.
 
 def reviews(request):
     if request.method == "POST":
-        form = ReviewForm(request.POST)
+        existing_data = Review.objects.get(pk=2)
+        print(existing_data)
+        form = ReviewForm(request.POST, instance=existing_data)
 
         if form.is_valid():  # is_valid will do 3 things.. 1) validiate the inputs (by default verifies not empty) 2) then will return True if valid 3) if data is valid it populates another field with valide data
-            print(form.cleaned_data['user_name'])
+            # data = form.cleaned_data
+            # print(data['user'], data['review'], data['rating'])
+            form.save()
+            # temp_review_data = Review(
+            #     user=data['user'], 
+            #     review=data['review'], 
+            #     rating=data['rating'])
+            # temp_review_data.save()
+            
             return HttpResponseRedirect("/thank-you")
 
     else:
