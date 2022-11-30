00b58c33c3c867aea0b380061cfe2ae318aa81fa
Andy <andythomas.py@gmail.com>
2022-11-29 23:04:08 -0600

    last save before class based view added

--- reviews/views.py
+++ reviews/views.py
@@ -8,19 +8,16 @@ from .models import Review
 
 def reviews(request):
     if request.method == "POST":
-        existing_data = Review.objects.get(pk=2)
+        existing_data = Review.objects.get(pk=2) # assign variable to entry which should be updated
         print(existing_data)
-        form = ReviewForm(request.POST, instance=existing_data)
+        form = ReviewForm(request.POST, instance=existing_data) # to update data entry include instance keyword and pass in entry to update
+
+        # form = ReviewForm(request.Post) # to add a new entry
 
         if form.is_valid():  # is_valid will do 3 things.. 1) validiate the inputs (by default verifies not empty) 2) then will return True if valid 3) if data is valid it populates another field with valide data
-            # data = form.cleaned_data
-            # print(data['user'], data['review'], data['rating'])
+            print(form)
             form.save()
-            # temp_review_data = Review(
-            #     user=data['user'], 
-            #     review=data['review'], 
-            #     rating=data['rating'])
-            # temp_review_data.save()
+
             
             return HttpResponseRedirect("/thank-you")
 
