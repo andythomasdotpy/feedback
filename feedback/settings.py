3f43ccffc910a5de9b4ef107bd3653abf1d43991
Andy <andythomas.py@gmail.com>
2022-11-30 16:18:53 -0600

    hide vars

--- feedback/settings.py
+++ feedback/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+# from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-$)%52-w#9p$tg*@_-zsh=l4kbewscw-vl*dv-k711-$m*-sng+"
+# SECRET_KEY = "django-insecure-$)%52-w#9p$tg*@_-zsh=l4kbewscw-vl*dv-k711-$m*-sng+"
+# SECRET_KEY = os.environ["SECRET_KEY"]
+with open(os.path.join(BASE_DIR, 'secret_key.txt')) as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
