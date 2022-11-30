e7a39f4a919a0dce70208d2e0f7f7a5fa834333c
Andy <andythomas.py@gmail.com>
2022-11-30 16:28:15 -0600

    hide vars

--- feedback/settings.py
+++ feedback/settings.py
@@ -22,8 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = "django-insecure-$)%52-w#9p$tg*@_-zsh=l4kbewscw-vl*dv-k711-$m*-sng+"
-# SECRET_KEY = os.environ["SECRET_KEY"]
 with open(os.path.join(BASE_DIR, 'secret_key.txt')) as f:
     SECRET_KEY = f.read().strip()
 
